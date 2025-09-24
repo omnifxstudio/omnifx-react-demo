8823abbb70aa3989c6dad688814bc8a6381313fc
Yashhfartade <yashfartade356@gmail.com>
2025-09-24 17:19:25 +0530

    fix: configure vite to correctly handle .riv file assets

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This new line tells Vite to treat .riv files as assets
+  assetsInclude: ['**/*.riv'], 
 })
